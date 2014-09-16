e1d11fd5ab421bf0d6850fad45bcf5473490ed79
Travis Webb <me@traviswebb.com>
2014-09-15 21:46:58 -0400

    added passport config file

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ module.exports = require('sails-generate-entities')({
     'api/models/Passport.js',
     'api/models/User.js',
     'api/controllers/AuthController.js',
+    'api/services/protocols/index.js',
+    'config/passport.js'
   ],
   classes: [
     'api/policies/passport.js',
     'api/services/passport.js',
-    'api/services/protocols/index.js',
     'api/services/protocols/local.js',
     'api/services/protocols/oauth.js',
     'api/services/protocols/oauth2.js',
