c7846846b00556a7848591867cbf402e5eada9e1
Travis Webb <me@traviswebb.com>
2014-10-30 17:15:23 -0400

    re-organized files, removed hook

---
+++ config/routes.js
@@ -0,0 +1,12 @@
+module.exports.routes = {
+  'get /login': 'AuthController.login',
+  'get /logout': 'AuthController.logout',
+  'get /register': 'AuthController.register',
+
+  'post /auth/local': 'AuthController.callback',
+  'post /auth/local/:action': 'AuthController.callback',
+
+  'get /auth/:provider': 'AuthController.provider',
+  'get /auth/:provider/callback': 'AuthController.callback',
+  'get /auth/:provider/:action': 'AuthController.callback'
+};
