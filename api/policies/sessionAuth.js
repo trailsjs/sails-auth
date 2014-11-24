5ded8e3ef384b1fd67805ef81f63bb206fb13cb7
Travis Webb <me@traviswebb.com>
2014-11-24 17:45:16 -0500

    return json object with 403

--- api/policies/sessionAuth.js
+++ api/policies/sessionAuth.js
@@ -15,5 +15,5 @@ module.exports = function(req, res, next) {
 
   // User is not allowed
   // (default res.forbidden() behavior can be overridden in `config/403.js`)
-  return res.forbidden('You are not permitted to perform this action.');
+  return res.forbidden(JSON.stringify({ error: 'You are not permitted to perform this action.' }));
 };
