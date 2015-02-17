04acdf779837fb58a26195c1b03d81950570c93e
Travis Webb <me@traviswebb.com>
2015-02-17 10:42:11 -0500

    remove username<-->email set

--- api/models/User.js
+++ api/models/User.js
@@ -7,7 +7,8 @@ module.exports = {
     username: {
       type: 'string',
       unique: true,
-      index: true
+      index: true,
+      notNull: true
     },
     email: {
       type: 'email',
@@ -32,16 +33,5 @@ module.exports = {
       user.gravatarUrl = this.getGravatarUrl();
       return user;
     }
-  },
-
-  /**
-   * Set username if only email is set
-   */
-  afterValidate: function (user, next) {
-    if (_.isEmpty(user.username) && !_.isEmpty(user.email)) {
-      user.username = user.email;
-    }
-
-    next();
   }
 };
